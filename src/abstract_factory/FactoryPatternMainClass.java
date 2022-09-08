293876dd00208010b2b5ddec3d909ad8ac887e0e
Susavan <susavanpal07@gmail.com>
2022-09-08 15:23:59 +0530

    Added the code for main class

--- src/abstract_factory/FactoryPatternMainClass.java
+++ src/abstract_factory/FactoryPatternMainClass.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 public class FactoryPatternMainClass {
 
     public static void main(String[] args) {
-        Optional<String> vehicle = Optional.of("car");
-        vehicle = Optional.ofNullable(null);
-        System.out.println(""+vehicle.map(s -> "This is " + s + "!").orElse("this is not car!"));
-
+       // first get factory instance from factory of factory ,if u want trainees
+       AbstractFactory abstractFactory=new AbstractFactoryProducer().getProfession(true);
+       Profession engg=abstractFactory.getProfession(("Engineer"));
+       engg.print();
 
     }
 
